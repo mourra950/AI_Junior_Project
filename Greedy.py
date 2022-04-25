5d91ded0a956ee50c0814d0bbb03e9b20637a5cc
OmarElbanna <elbanna16@gmail.com>
2022-04-25 23:05:02 +0200

    added path cost to greedy.py

--- Greedy.py
+++ Greedy.py
@@ -6,16 +6,18 @@ g.add_node(2,h=0,id=2)
 g.add_edge(1,2,weight=6)
 g.add_edge(1,3,weight=4)
 g.add_node(4,h=0,id=1)
-g.add_edge(3,4,weight=6)
+g.add_edge(3,4,weight=10)
 
 def getPath(node):
     path=[]
+    cost=0
     path.append(node)
     while not(g.nodes[node]['parent'] is None):
         path.append(g.nodes[node]['parent'])
+        cost += nx.get_edge_attributes(g, 'weight')[(g.nodes[node]['parent'],node)]
         node=g.nodes[node]['parent']
     path.reverse()
-    return path
+    return path,cost
 
 def getH(node):
     return g.nodes[node]['h']
@@ -33,14 +35,13 @@ def greedy(start,goal):
             visited.append(x)
             fringe.pop(0)
             if x in goal:
-                print(x) # get path completed still the path cost
+                print("Goal is "+ str(x),sep=" ")
                 print(getPath(x))
                 return
             for y in nx.all_neighbors(g,x):
                 if not(y in visited):
                     fringe.append(y)
                     nx.set_node_attributes(g, {y: x}, name="parent")
-
 
         fringe.sort(key=getH)
 
