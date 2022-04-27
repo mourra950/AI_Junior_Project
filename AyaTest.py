2c5fa9c827c30044f3c9edd9c2cdb3700a2bb6f8
Aya-Ahmed-Gamal <19P1689@eng.asu.edu.eg>
2022-04-27 04:16:16 +0200

    printing  visited nodes
    print the visited nodes and trying to draw the graph

--- AyaTest.py
+++ AyaTest.py
@@ -1,11 +1,11 @@
 
 import networkx as nx
 import matplotlib.pyplot as plt
-G=nx.Graph()
-G.add_node("Tom")
+#G=nx.Graph()
+#G.add_node("Tom")
 
-G.add_node("Jerry")
-G.add_edge("Tom","Jerry",weight=5) #It creates nodes if they dont exist
+#G.add_node("Jerry")
+#G.add_edge("Tom","Jerry",weight=5) #It creates nodes if they dont exist
 #print(list(G))
 #for i in G:
 #    print(i)
@@ -16,15 +16,15 @@ G.add_edge("Tom","Jerry",weight=5) #It creates nodes if they dont exist
 #G2.add_node("Lucy")
 #G2.add_node("Betty")
 #len(G)=nx.number_of_nodes(G)
-plt.figure(1) #to make the G graph in a seperate figure
-pos=nx.spring_layout(G)
-nx.draw(G,pos,with_labels=True,font_size=10,font_color="#2A659A",bbox=dict(facecolor="#24517A"))
-nx.draw_networkx_edge_labels(G,pos,edge_labels=nx.get_edge_attributes(G,'weight'))
+#plt.figure(1) #to make the G graph in a seperate figure
+#pos=nx.spring_layout(G)
+#nx.draw(G,pos,with_labels=True,font_size=10,font_color="#2A659A",bbox=dict(facecolor="#24517A"))
+#nx.draw_networkx_edge_labels(G,pos,edge_labels=nx.get_edge_attributes(G,'weight'))
 #plt.figure(2)
 #nx.draw(G2,node_color="red")
 
 #plt.show() 
-
+visited=[]
 def path_cost(path):
     total_cost=0
     for(node,cost) in path:
@@ -35,7 +35,7 @@ def path_cost(path):
 #print(path_cost(path))
     
 def ucs(graph,s,g):
-    visited=[]
+    #visited=[]
     queue=[[(s,0)]]
     while queue:
         queue.sort(key=path_cost)
@@ -53,7 +53,13 @@ def ucs(graph,s,g):
                 new_path.append((node2,cost))
                 queue.append(new_path)
         
+def ayhaga():
+    
+    print("Visited nodes are ")
+    for i in visited:
+        print (i)
 
+        
         
 #Testing the ucs function    
 graph={
@@ -65,6 +71,15 @@ graph={
 'G':[],
     }   
 
-solution=ucs(graph,'C','G')
+solution=ucs(graph,'A','G')
 print("Solution is ", solution)
 print("Path Cost is ", path_cost(solution)[0])
+ayhaga()
+nodes=[("S","A"),("S","B"),("S","D"),("A","C"),("B","D"),("C","D"),("C","G"),("D","G")]
+graph=nx.Graph()
+graph.add_edges_from(nodes)
+#graph=nx.Graph()
+nx.draw(graph,with_labels=True,font_size=10,font_color="#2A659A",bbox=dict(facecolor="#24517A"))
+#nx.draw_networkx_edge_labels(graph,edge_labels=nx.get_edge_attributes(graph,'weight'))
+
+plt.show()
